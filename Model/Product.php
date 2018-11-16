e770d9319c8bb9a15703c454059978a68153a9f6
Evan Burrell <evan.burrell@ampersandit.co.uk>
2018-11-16 15:42:49 +0000

    Update for configuration value in admin panel

--- Model/Product.php
+++ Model/Product.php
@@ -4,8 +4,20 @@ namespace EB\AwesomePlugin\Model;
 
 class Product
 {
+    private $scopeConfig;
+
+    public function __construct(\Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig) {
+        $this->scopeConfig = $scopeConfig;
+    }
+
     public function afterGetName(\Magento\Catalog\Model\Product $subject, $result)
     {
+        $isModuleEnabled = $this->scopeConfig->getValue('ampersand/general/awesomeplugin', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+
+        if (!$isModuleEnabled) {
+            return $result;
+        }
+
         return "Awesome " . $result;
     }
 }
